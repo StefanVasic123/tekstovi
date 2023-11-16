15e044129bece5db274cce09b16eec5fb2cae972
Stefan Vasic <vasicstefan123@gmail.com>
2023-11-16 19:47:03 +0100

    copy to clipboard button on single post implemented

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -3,6 +3,32 @@ import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Layout from '@/app/components/layout';
 
+const CopyToClipboardIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    className='w-6 h-6 text-gray-500 hover:text-gray-700 transition duration-300'
+    {...props}
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M9 5h7a2 2 0 012 2v12a2 2 0 01-2 2H9a2 2 0 01-2-2V7a2 2 0 012-2z'
+    />
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M9 5V3a1 1 0 011-1h7a1 1 0 011 1v2'
+    />
+  </svg>
+);
+
 interface Post {
   id: string;
   title: string;
@@ -14,6 +40,7 @@ interface Post {
 
 const PostPage: React.FC = () => {
   const [post, setPost] = useState<Post | null>(null);
+  const [isCopied, setIsCopied] = useState(false);
   const path = usePathname();
   const parts = path.split('/');
   const id = parts[parts.length - 1];
@@ -39,9 +66,26 @@ const PostPage: React.FC = () => {
     }
   }, [id]);
 
+  const copyLink = () => {
+    const fullUrl = window.location.href;
+    navigator.clipboard.writeText(fullUrl);
+    setIsCopied(true);
+
+    // Reset the copied state after a short delay
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
   if (!post) {
     return <p>Loading post...</p>;
   }
+
+  const notification = isCopied && (
+    <div className='bg-gray-500 text-white p-2 absolute top-0 right-0 mt-2 mr-2 rounded'>
+      Copied to clipboard!
+    </div>
+  );
 
   const videoFrame = (
     <div className='flex items-center justify-center max-w-screen-lg mx-auto my-6'>
@@ -62,7 +106,14 @@ const PostPage: React.FC = () => {
 
   const postContent = (
     <div className='bg-white p-4 shadow-md max-w-screen-lg mx-auto my-6 relative'>
-      <h1 className='text-3xl font-semibold text-center mb-2'>{post.title}</h1>
+      {notification}
+      <h1 className='text-3xl font-semibold text-center mb-2'>
+        {post.title}
+        <CopyToClipboardIcon
+          onClick={copyLink}
+          className='ml-2 cursor-pointer w-6 h-6 text-gray-500 hover:text-gray-700 transition duration-300 inline-block'
+        />
+      </h1>
       <p className='text-gray-700 text-lg my-4'>{postText(post.content)}</p>
       <div className='flex justify-between'>
         <p className='text-blue-600 font-semibold'>{post.genre}</p>
